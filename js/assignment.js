75c4ef2c680305a016239d86c5235395ed3a4d9b
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2025-01-07 17:14:39 -0500

    add functionality

--- js/assignment.js
+++ js/assignment.js
@@ -13,6 +13,13 @@ function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
     
     // Write your code below
+    if(age >= 21){
+        paragraph.innerText = "You can vote and purchase alchol." 
+    }else if(age >= 18){
+        paragraph.innerText = "You can vote, but you cannot purchase alcohol."
+    }else if(age < 18){
+        paragraph.innerText = "You cannot vote and you cannot purchase alcohol."
+    }
 
 }
 
